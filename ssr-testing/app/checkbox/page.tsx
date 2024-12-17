3e5ca6efc76025aea51b477d9bda0dddf7424967
paullfhc <leflohicpaul@gmail.com>
2024-12-17 21:59:26 +0100

    ensemble des modifications aportées à la page d'implémentation de la checkbox

--- ssr-testing/app/checkbox/page.tsx
+++ ssr-testing/app/checkbox/page.tsx
@@ -1,10 +1,38 @@
+'use client';
+
 import * as React from 'react';
 import * as Checkbox from '@radix-ui/react-checkbox';
 
 export default function Page() {
+  // État pour le div
+  const [isDivChecked, setIsDivChecked] = React.useState(false);
+
+  // État pour la case à cocher
+  const [isCheckboxChecked, setIsCheckboxChecked] = React.useState(false);
+
+  // Fonction pour alterner l’état du div
+  const handleDivClick = () => {
+    console.log('Div clicked:', !isDivChecked);
+    setIsDivChecked((prev) => !prev);
+  };
+
+  // Fonction pour alterner l’état de la checkbox
+  const handleCheckedChange = (newChecked: boolean) => {
+    console.log('Checkbox checked:', newChecked);
+    setIsCheckboxChecked(newChecked);
+  };
+
   return (
-    <Checkbox.Root>
-      [ <Checkbox.Indicator>✔</Checkbox.Indicator> ]
-    </Checkbox.Root>
+    <form>
+      <div onClick={handleDivClick}> {/* Alterne l’état du div */}
+        <Checkbox.Root 
+          checked={isCheckboxChecked} 
+          onCheckedChange={handleCheckedChange} // Alterne l’état de la checkbox
+        >
+          [ <Checkbox.Indicator>✔</Checkbox.Indicator> ]
+        </Checkbox.Root>
+        <span>test</span>
+      </div>
+    </form>
   );
 }
